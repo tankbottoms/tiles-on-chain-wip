f30307f06a57e2fce5f2ca4b60e01b7ebc9acae2
cptspacecadet <m@tankbottoms.xyz>
2022-04-06 04:04:44 +0500

    update

--- scripts/data/crawl.ts
+++ scripts/data/crawl.ts
@@ -12,7 +12,7 @@ async function main() {
     const data: any = {};
     for (const file of files) {
         const fileData = fs.readFileSync(getPath(file), 'utf8').toString();
-        const _addresses = (fileData.match(/0x[0-9a-fA-F]{40,40}[^0-9a-fA-F]/g) || []).map(a => a.slice(0, 42));
+        const _addresses = (fileData.match(/0x[0-9a-fA-F]{40,40}[^0-9a-fA-F]/g) || []).map(a => a.slice(0, 42).toLowerCase());
         for (const address of _addresses) {
             addresses.add(address);
         }
